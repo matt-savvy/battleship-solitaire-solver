9212acfa931219216089fdf437326195e5c483bc
Matt <matt@1-800-rad-dude.com>
2024-10-30 17:32:32 -0400

    Remove hello world

--- test/battleship_solitaire_solver_test.exs
+++ test/battleship_solitaire_solver_test.exs
@@ -1,8 +1,3 @@
 defmodule BattleshipSolitaireSolverTest do
   use ExUnit.Case
-  doctest BattleshipSolitaireSolver
-
-  test "greets the world" do
-    assert BattleshipSolitaireSolver.hello() == :world
-  end
 end
