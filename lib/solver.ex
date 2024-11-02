c311aa3f6b8a3b3771ca5c24db617fc531a951bd
Matt <matt@1-800-rad-dude.com>
2024-11-02 18:38:47 -0400

    Update Solver, add get_all_formations

--- lib/solver.ex
+++ lib/solver.ex
@@ -2,4 +2,29 @@ defmodule BattleshipSolitaireSolver do
   @moduledoc """
   Documentation for `BattleshipSolitaireSolver`.
   """
+  alias BattleshipSolitaireSolver.Formation
+
+  def get_all_formations(grid_size, ships) do
+    formation = Formation.new()
+    do_get_all_formations(grid_size, formation, ships)
+  end
+
+  defp do_get_all_formations(grid_size, %Formation{} = formation, [] = _ships) do
+    [formation]
+  end
+
+  defp do_get_all_formations(grid_size, %Formation{} = formation, [ship | rest_ships]) do
+    possible_locs(grid_size, ship)
+    |> Enum.flat_map(fn loc ->
+      # update formation
+      do_get_all_formations(grid_size, formation, rest_ships)
+    end)
+    |> List.first()
+  end
+
+  def possible_locs(grid_size, ship) do
+    for col <- 1..grid_size, row <- 1..grid_size do
+      {col, row}
+    end
+  end
 end
